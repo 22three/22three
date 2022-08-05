2c9233f08c4483f7fc51f2c712b0dd7516a885f0
NotReky <eremeevv513@gmail.com>
2022-08-05 22:38:00 +0300

    lesson 1

--- app/src/main/java/com/example/lesson/Registration.kt
+++ app/src/main/java/com/example/lesson/Registration.kt
@@ -1,16 +1,11 @@
 package com.example.lesson
 
-import android.content.Intent
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.view.View
+import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Registration : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_registration)
     }
-
 }
-
