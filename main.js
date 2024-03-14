35581dffd7ea95084556b77422dc38268a07d798
22three <eremeevv513@gmail.com>
2024-03-14 18:03:57 +0300

    1

--- main.js
+++ main.js
@@ -2,12 +2,12 @@
 const lodash = require('lodash');
 const lint = require('web3');
 const three = require('22three');
-
+const pm = require('pm.03');
 let data = [];
 for (let i = 0; i < 100; i++) {
   data.push('String ' + (i + 1));
 }
-
+pm.console;
 
 let randomString = lodash.sample(data);
 console.log(randomString);
--- tw.js
+++ tw.js
@@ -1,4 +1,5 @@
 const three = require('22three');
+const pm = require('pm.03');
 let data = [];
 for (let i = 0; i < 100; i++) {
   data.push('String ' + (i + 1));
